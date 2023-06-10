b9198987f8edefef271fa279c830170b51c465af
linuxing3 <linuxing3@qq.com>
2023-06-11 00:43:37 +0800

    [feat]

--- src/raytracer/layer.rs
+++ src/raytracer/layer.rs
@@ -1,6 +1,7 @@
 use std::{
     f32::consts::{FRAC_1_PI, PI},
     ptr::null_mut,
+    sync::Arc,
 };
 
 use crate::fly_camera::{camera_orientation, FlyCameraController};
@@ -11,17 +12,20 @@ use imgui::TextureId;
 use nalgebra_glm::{acos, atan2, dot, Vec3};
 
 pub struct Layer<'a> {
-    texture_id : imgui::TextureId,
-    pub size : [f32; 2],
-    pub title : &'a str,
-    pub file_path : &'a str,
-    imgbuf : *mut XImageBuffer,
-    pub camera_controller : FlyCameraController,
+    texture_id: imgui::TextureId,
+    pub size: [f32; 2],
+    pub title: &'a str,
+    pub file_path: &'a str,
+    imgbuf: *mut XImageBuffer,
+    pub camera_controller: FlyCameraController,
 }
 
 impl<'a> Layer<'a> {
-    pub fn new(size : [f32; 2], title : &'a str, file_path : &'a str) -> Self {
-
+    pub fn new(
+        size: [f32; 2],
+        title: &'a str,
+        file_path: &'a str,
+    ) -> Self {
         let camera_controller = FlyCameraController::default();
 
         let [width, height] = size;
@@ -30,14 +34,12 @@ impl<'a> Layer<'a> {
 
         // A redundant loop to demonstrate reading image data
         for j in 0..height as u32 {
-
             for i in 0..width as u32 {
-
                 let pixel = new_imgbuf.get_pixel_mut(i, j);
 
-                let x = i as f32 / width;
+                let x = (i as f32 / width) * 2.0 - 1.0;
 
-                let y = j as f32 / height;
+                let y = (j as f32 / height) * 2.0 - 1.0;
 
                 *pixel = Self::per_pixel(x, y);
             }
@@ -59,29 +61,25 @@ impl<'a> Layer<'a> {
 
     pub fn register_texture(
         &mut self,
-        device : &wgpu::Device,
-        queue : &wgpu::Queue,
-        renderer : &mut imgui_wgpu::Renderer,
+        device: &wgpu::Device,
+        queue: &wgpu::Queue,
+        renderer: &mut imgui_wgpu::Renderer,
     ) -> Option<TextureId> {
-
-        let imgbuf_boxed = unsafe {
-
-            Box::from_raw(self.imgbuf)
-        };
+        let imgbuf_boxed = unsafe { Box::from_raw(self.imgbuf) };
 
         let (width, height) = imgbuf_boxed.dimensions();
 
         let img = DynamicImage::from(*imgbuf_boxed);
 
-        let bytes : &[u8] = &img.to_rgba8();
+        let bytes: &[u8] = &img.to_rgba8();
 
         let size = wgpu::Extent3d {
             width,
             height,
-            depth_or_array_layers : 1,
+            depth_or_array_layers: 1,
         };
 
-        let imgui_texture : _ =
+        let imgui_texture: _ =
             WgpuTexture::new_imgui_texture(&device, &queue, &renderer, &bytes, size);
 
         self.texture_id = renderer.textures.insert(imgui_texture);
@@ -89,22 +87,22 @@ impl<'a> Layer<'a> {
         Some(self.texture_id)
     }
 
-    pub fn texture_id(&mut self) -> &imgui::TextureId { &self.texture_id }
+    pub fn texture_id(&mut self) -> &imgui::TextureId {
+        &self.texture_id
+    }
 
     pub fn img_buf(&mut self) -> XImageBuffer {
-
-        let imgbuf_boxed = unsafe {
-
-            Box::from_raw(self.imgbuf)
-        };
+        let imgbuf_boxed = unsafe { Box::from_raw(self.imgbuf) };
 
         let mut imgbuf = *imgbuf_boxed;
 
         imgbuf
     }
 
-    pub fn render(&mut self, ui : &mut imgui::Ui) {
-
+    pub fn render(
+        &mut self,
+        ui: &mut imgui::Ui,
+    ) {
         let window = ui.window(self.title);
 
         let mut new_imgui_region_size = None;
@@ -120,7 +118,6 @@ impl<'a> Layer<'a> {
         window
             .size(self.size, imgui::Condition::FirstUseEver)
             .build(|| {
-
                 new_imgui_region_size = Some(ui.content_region_avail());
 
                 ui.text(format!("ray origin x: {}", ray.origin.x));
@@ -139,10 +136,11 @@ impl<'a> Layer<'a> {
             });
     }
 
-    pub fn resize(&mut self, new_size : [f32; 2]) {
-
+    pub fn resize(
+        &mut self,
+        new_size: [f32; 2],
+    ) {
         if self.size != new_size {
-
             self.size = new_size;
         }
 
@@ -154,14 +152,12 @@ impl<'a> Layer<'a> {
 
         // A redundant loop to demonstrate reading image data
         for j in 0..height as u32 {
-
             for i in 0..width as u32 {
-
                 let pixel = new_imgbuf.get_pixel_mut(i, j);
 
-                let x = i as f32 / width;
+                let x = (i as f32 / width) * 2.0 - 1.0;
 
-                let y = j as f32 / height;
+                let y = (j as f32 / height) * 2.0 - 1.0;
 
                 *pixel = self.update_pixel(x, y);
             }
@@ -170,46 +166,52 @@ impl<'a> Layer<'a> {
         self.imgbuf = Box::into_raw(Box::new(new_imgbuf));
     }
 
-    pub fn update_pixel(&mut self, x : f32, y : f32) -> Rgb<u8> {
+    pub fn update_pixel(
+        &mut self,
+        x: f32,
+        y: f32,
+    ) -> Rgb<u8> {
+        let origin = Vec3::new(0.0, 0.0, 2.0);
 
-        let orientation = camera_orientation(&self.camera_controller);
-
-        let origin = self.camera_controller.position;
-
-        let direction = orientation.forward - origin;
+        let direction = Vec3::new(x, y, -1.0);
 
         let ray = Ray { origin, direction };
 
         let radius = 0.5_f32;
 
-        let a = dot(&ray.direction, &ray.direction);
+        // closeshit
+        let t = Self::ray_intersect_circle(&ray, radius);
 
-        let b = 2.0 * dot(&ray.origin, &ray.direction);
-
-        let c = dot(&ray.origin, &ray.origin) - radius * radius;
-
-        let discriminant = b * b - 4.0 * a * c;
+        // Normal
 
         // println!(" Coords: [{}, {}] ", x, y);
         // println!(" Color:  [{}, {}, {} -> {}] ", a, b, c, discriminant);
+        let hit_color = Rgb([15.0 as u8, 18.0 as u8, 18.0 as u8]);
 
-        match discriminant >= 0.0 {
-            true => Rgb([125.0 as u8, 18.0 as u8, 18.0 as u8]),
-            false => Rgb([(x * 255.0) as u8, (y * 255.0) as u8, 55.0 as u8]),
+        let background_color = Rgb([(x * 255.0) as u8, (y * 255.0) as u8, 1.0 as u8]);
+
+        match t >= 0.0 {
+            true => hit_color,
+            false => background_color,
         }
     }
 
-    pub fn per_pixel(x : f32, y : f32) -> Rgb<u8> {
-
-        let ray = Ray::new2(x, y);
+    pub fn per_pixel(
+        x: f32,
+        y: f32,
+    ) -> Rgb<u8> {
+        let ray_sptr = Arc::new(Ray::new2(x, y));
 
         let radius = 0.5_f32;
 
-        let t = Self::ray_intersect_circle(ray, radius);
+        // t
+        let t = Self::ray_intersect_circle(&ray_sptr, radius);
+
+        let sphere = Sphere::new(glm::vec3(0.0, 0.0, -1.0), radius, 1);
 
         // println!(" Coords: [{}, {}] ", x, y);
         // println!(" Color:  [{}, {}, {} -> {}] ", a, b, c, discriminant);
-        let hit_color = Rgb([125.0 as u8, 128.0 as u8, 128.0 as u8]);
+        let hit_color = Rgb([(x * 255.0) as u8, (y * 255.0) as u8, 0.0 as u8]);
 
         let background_color = Rgb([(x * 255.0) as u8, (y * 255.0) as u8, 55.0 as u8]);
 
@@ -219,8 +221,12 @@ impl<'a> Layer<'a> {
         }
     }
 
-    pub fn set_pixel_with_art_style(x : u32, y : u32, scalex : f32, scaley : f32) -> Rgb<u8> {
-
+    pub fn set_pixel_with_art_style(
+        x: u32,
+        y: u32,
+        scalex: f32,
+        scaley: f32,
+    ) -> Rgb<u8> {
         let cx = y as f32 * scalex - 1.5;
 
         let cy = x as f32 * scaley - 1.5;
@@ -232,7 +238,6 @@ impl<'a> Layer<'a> {
         let mut i = 0;
 
         while i < 255 && z.norm() <= 2.0 {
-
             z = z * z + c;
 
             i += 1;
@@ -242,11 +247,10 @@ impl<'a> Layer<'a> {
     }
 
     pub fn new_from_image_buffer(
-        width : u32,
-        height : u32,
-        path : &str,
+        width: u32,
+        height: u32,
+        path: &str,
     ) -> Result<XImageBuffer, TextureError> {
-
         // Create a new ImgBuf with width: imgx and height: imgy
         let imgbuf = ImageBuffer::new(width, height);
 
@@ -256,27 +260,31 @@ impl<'a> Layer<'a> {
         Ok(imgbuf)
     }
 
-    pub fn ray_intersect_circle(ray : Ray, radius : f32) -> f32 {
-
+    pub fn ray_intersect_circle(
+        ray: &Ray,
+        radius: f32,
+    ) -> f32 {
         let a = dot(&ray.direction, &ray.direction);
 
-        let b = 2.0 * dot(&ray.origin, &ray.direction);
+        let b = dot(&ray.origin, &ray.direction);
 
         let c = dot(&ray.origin, &ray.origin) - radius * radius;
 
-        let discriminant = b * b - 4.0 * a * c;
+        let discriminant = b * b - a * c;
 
-        discriminant
+        if discriminant < 0.0 {
+            return -1.0;
+        }
+        return (-b - num::Float::sqrt(discriminant)) / a;
     }
 
     pub fn ray_intersect_sphere(
-        ray : &mut Ray,
-        sphere : Sphere,
-        tmin : f32,
-        tmax : f32,
-        hit : Intersection,
+        ray: &mut Ray,
+        sphere: Sphere,
+        tmin: f32,
+        tmax: f32,
+        hit: Intersection,
     ) -> bool {
-
         let oc = ray.origin - sphere.center.xyz();
 
         let a = dot(&ray.direction, &ray.direction);
@@ -288,11 +296,9 @@ impl<'a> Layer<'a> {
         let discriminant = b * b - 4.0 * a * c;
 
         if discriminant > 0.0 {
-
             let mut t = (-b - num::Float::sqrt(discriminant)) / a;
 
             if t < tmax && t > tmin {
-
                 // *hit = Self::sphere_intersection(*ray, sphere, t);
 
                 return true;
@@ -301,7 +307,6 @@ impl<'a> Layer<'a> {
             t = (-b + num::Float::sqrt(discriminant)) / a;
 
             if t < tmax && t > tmin {
-
                 // *hit = sphere_intersection(*ray, sphere, t);
 
                 return true;
@@ -311,8 +316,11 @@ impl<'a> Layer<'a> {
         return false;
     }
 
-    pub fn sphere_intersection(ray : Ray, sphere : Sphere, t : f32) -> Intersection {
-
+    pub fn sphere_intersection(
+        ray: Ray,
+        sphere: Sphere,
+        t: f32,
+    ) -> Intersection {
         let p = Self::ray_point_at_parameter(ray, t);
 
         let n = (1.0 / sphere.radius) * (p - sphere.center.xyz());
@@ -328,8 +336,10 @@ impl<'a> Layer<'a> {
         return Intersection { p, n, u, v, t };
     }
 
-    pub fn ray_point_at_parameter(ray : Ray, t : f32) -> Vec3 {
-
+    pub fn ray_point_at_parameter(
+        ray: Ray,
+        t: f32,
+    ) -> Vec3 {
         return ray.origin + t * ray.direction;
     }
 }
